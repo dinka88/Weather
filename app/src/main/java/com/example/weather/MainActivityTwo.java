07755f16029820da55f64d8ab8e10f29b8c231bc
dinka88 <evanika88@gmail.com>
2020-10-28 22:35:31 +0300

    hw-2

---
+++ app/src/main/java/com/example/weather/MainActivityTwo.java
@@ -0,0 +1,28 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_two);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivityTwo.this, MainActivity.class));
+//                setContentView(R.layout.activity_main);
+            }
+        });
+
+    }
+
+}
